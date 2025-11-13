75635b318db7f19c92af80e85e0220a0cab7e301
apilaka <apilaka@yahoo.ca>
2025-11-13 06:49:31 -0500

    first commit basic restaruant servicde  works

---
+++ src/main/java/com/pilaka/restaurant_listing/RestaurantListingApplication.java
@@ -0,0 +1,14 @@
+package com.pilaka.restaurant_listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class RestaurantListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantListingApplication.class, args);
+	}
+
+}
